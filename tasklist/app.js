6e3e62435d1ccffe498ba3397c4ac0f936aaf36c
Weronika Kansy <weronika.kansy@gmail.com>
2021-07-06 23:26:07 +0200

    UI elements targeted with querySelector and getElementById

---
+++ tasklist/app.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('task-form');
+const taskInput = document.getElementById('task');
+const filterInput = document.getElementById('filter');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');

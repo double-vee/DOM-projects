d10eca8ce741284278b03bb24fb5e9e8fa4a287e
Weronika Kansy <weronika.kansy@gmail.com>
2021-07-08 00:02:44 +0200

    Form event handler declared: tasks added to taskList on submit

--- tasklist/app.js
+++ tasklist/app.js
@@ -3,3 +3,30 @@ const taskInput = document.getElementById('task');
 const filterInput = document.getElementById('filter');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
+
+loadEventListeners();
+
+function loadEventListeners() {
+  form.addEventListener('submit', addTask)
+}
+
+function addTask(e) {
+  if(taskInput.value === ''){
+    alert('Add a task');
+  }
+
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  li.innerText = taskInput.value;
+
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fas fa-times"></i>';
+  li.appendChild(link);
+  
+  taskList.appendChild(li);
+
+  taskInput.value = '';
+
+  e.preventDefault();
+}
